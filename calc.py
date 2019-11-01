220baf85363723acf47045095ab29756c599655f
Rupashri <rupashri.balram.7@gmail.com>
2019-11-01 13:12:10 +0530

    Adding area calculataor python program to master branch

---
+++ calc.py
@@ -0,0 +1,9 @@
+#program to calculate area with user-entered dimensions
+
+print ("Let's find the area of a rectangle")
+length=input("Enter the length :")
+breadth=input("Enter the breadth :")
+
+area=length*breadth
+
+print ("Area of the rectangle is :",area)
